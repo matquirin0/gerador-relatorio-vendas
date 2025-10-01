acffb145568c02755ebd4cc80a1bea3f85d415b7
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-10-01 15:42:31 -0300

    Add: .classpath, .project, .settings, .bin, .src

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import services.LeitorVenda;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		LeitorVenda leitorVenda = new LeitorVenda();
+		
+	}
+
+}
