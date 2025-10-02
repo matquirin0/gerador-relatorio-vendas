9b5b352b283d67e4cb8b4800877738c7a4a302ce
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-10-01 22:19:46 -0300

    Fix: Adicionando o leitor de arquivo no Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.util.Scanner;
+
 import services.LeitorVenda;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		LeitorVenda leitorVenda = new LeitorVenda();
+		try ( Scanner sc = new Scanner(System.in)){
+			
+		// Selecionando o arquivo
+		System.out.println("Digite o caminho do arquivo: ");
+		String arquivo = sc.nextLine();
 		
+		LeitorVenda leitorVenda = new LeitorVenda();
+		leitorVenda.LeitorDeArquivo(arquivo);
+		System.out.println(leitorVenda.getVendas().toString());
+		}
 	}
-
 }
