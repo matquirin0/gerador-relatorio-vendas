165fac6d4b28a65fec2ff4b325a25ce32ae0ca15
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-10-01 22:56:12 -0300

    Add: Criação do construtor padrão e método de cálculo de faturamento total

--- src/services/AnalisadorVenda.java
+++ src/services/AnalisadorVenda.java
@@ -1,5 +1,22 @@
 package services;
 
+import java.util.List;
+
+import entities.Venda;
+
 public class AnalisadorVenda {
 
+	List<Venda> vendas;
+
+	public AnalisadorVenda() {
+
+	}
+
+	public double calcularFaturamentoTotal(List<Venda> vendas) {
+		double totalFaturamento = 0.0;
+		for (Venda v : vendas) {
+			totalFaturamento += v.getPrecoUnitario() * v.getQuantidade();
+		}
+		return totalFaturamento;
+	}
 }
